ec1a4749a26cfa88bd66db80209afbfea7e6bd4a
zigeoro <149702130+zigeoro@users.noreply.github.com>
2024-01-07 16:45:28 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,18 +1,11 @@
-
 for(let i = 1; i <= 100; i ++)
-
-  if(i%3 === 0)
-  console.log("3の倍数です");
-
-  else if(i%5 === 0)
-  
-  console.log("5の倍数です");
-  
-  else if(i%15 === 0)
-  
+  if(i%15 === 0){
   console.log("3と5の倍数です");
-  
-  else
-  
+  }else if(i%3 === 0){
+  console.log("3の倍数");
+  }else if(i%5 === 0){
+  console.log("5の倍数");
+  }else{
   console.log(i);
+  }
   
