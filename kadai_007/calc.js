257f0d4b719e9ac828322153d0051a44450cbfb2
zigeoro <149702130+zigeoro@users.noreply.github.com>
2024-01-14 21:03:27 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,7 +1,7 @@
 let num = Math.floor(Math.random() * 100)
 
 
-  if(num%15 === 0)
+  if(num % 15 === 0)
   {
   console.log("3と5の倍数です");
   }
@@ -18,21 +18,4 @@ let num = Math.floor(Math.random() * 100)
   }
   
 
-  for(let i = 1; i <= 100; i ++){
-
-    console.log( i );
-    if(i%3 === 0){
-    console.log("3の倍数です");
-    }
-    else if(i%5 === 0)
-    {
-    console.log("5の倍数です");
-    }
-    else if(i%15 === 0)
-    {
-    console.log("3と5の倍数です");
-    }
-    else{
-    console.log("3の倍数でも5の倍数でもありません");
-    }
-    }
+  
